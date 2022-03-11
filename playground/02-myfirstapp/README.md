fa64e5c4e0a69a9d435726e12e4c85efbfcff3d4
solygambas <51904909+solygambas@users.noreply.github.com>
2022-03-11 07:26:32 +0100

    add basic syntax

---
+++ playground/02-myfirstapp/README.md
@@ -0,0 +1,12 @@
+# Getting started with Kotlin
+
+A quick intro to Kotlin syntax.
+
+## Features
+
+- setting up a Kotlin project in IntelliJ IDEA.
+- adding a main function.
+- using loops and if statements.
+- using the Kotlin interactive shell.
+
+Based on [Head First Kotlin: A Brain-Friendly Guide](https://www.amazon.com/Head-First-Kotlin-Brain-Friendly-Guide/dp/1491996692) by David Griffiths and Dawn Griffiths (2019).
--- playground/02-myfirstapp/src/main/kotlin/Main.kt
+++ playground/02-myfirstapp/src/main/kotlin/Main.kt
@@ -1,14 +1,23 @@
 fun main() {
     //print("Pow!") // same line
-    var x = 1
-    println("Before the loop. x = $x") // new line
-    while(x < 4) {
-        println("In the loop. x = $x")
-        x += 1
-    }
-    println("After the loop. x = $x") // 4
-    var y = 2
+    //var x = 1
+    //println("Before the loop. x = $x") // new line
+    //while(x < 4) {
+        //println("In the loop. x = $x")
+       // x += 1
+    //}
+    //println("After the loop. x = $x") // 4
+    // var y = 2
 
     // if statements
-    println(if (x > y) "x is greater than y" else "x is not greater than y")
+    //println(if (x > y) "x is greater than y" else "x is not greater than y")
+
+    // code magnets
+    var x = 1
+    while (x < 3) {
+        print(if(x == 1) "Yab" else "Dab")
+        print("ba")
+        x += 1
+    }
+    if (x == 3) print("Do") // YabbaDabbaDo
 }
