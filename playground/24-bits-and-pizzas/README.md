64db47b74b03683c3592efa6f9dcdfbe4a64bf2b
solygambas <51904909+solygambas@users.noreply.github.com>
2022-03-27 15:50:57 +0200

    add order fragment

--- playground/24-bits-and-pizzas/README.md
+++ playground/24-bits-and-pizzas/README.md
@@ -2,14 +2,14 @@
 
 A create order screen to discover how to implement Material views.
 
-<!-- <p align="center">
+<p align="center">
 <img src="screenshot.png" style="width:528px;max-width: 100%;">
-</p> -->
+</p>
 
 ## Features
 
-- adding a toolbar that can scroll, expand and collapse.
+- adding a toolbar that can scroll, collapse and expand.
 - adding radio buttons, chips, and a floating action button.
-- displaying pop-up messages with snackbars.
+- displaying pop-up messages with toasts and snackbars.
 
 Based on [Head First Android Development: A Brain-Friendly Guide](https://www.amazon.com/Head-First-Android-Development-Brain-Friendly/dp/1449362184) by David Griffiths and Dawn Griffiths (2021).
--- playground/24-bits-and-pizzas/app/src/main/java/com/hfad/bitsandpizzas/OrderFragment.kt
+++ playground/24-bits-and-pizzas/app/src/main/java/com/hfad/bitsandpizzas/OrderFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.chip.Chip
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * A simple [Fragment] subclass.
@@ -23,6 +28,26 @@ class OrderFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_order, container, false)
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_group)
+            val pizzaType = pizzaGroup.checkedRadioButtonId
+            if (pizzaType == -1) {
+                val text = "You need to choose a pizza type"
+                Toast.makeText(activity, text, Toast.LENGTH_LONG).show()
+            } else {
+                var text = (when (pizzaType) {
+                    R.id.radio_diavolo -> "Diavolo pizza"
+                    else -> "Funghi pizza"
+                })
+                val parmesan = view.findViewById<Chip>(R.id.parmesan)
+                text += if (parmesan.isChecked) ", extra parmesan" else ""
+                val chiliOil = view.findViewById<Chip>(R.id.chili_oil)
+                text += if (chiliOil.isChecked) ", extra chili oil" else ""
+                Snackbar.make(fab, text, Snackbar.LENGTH_LONG).show()
+            }
+        }
         return view
     }
 
