d130ab41304f18fd7e6a9b4793a97f311231b793
solygambas <51904909+solygambas@users.noreply.github.com>
2022-03-28 08:20:11 +0200

    add view model and factory class

--- playground/25-guessing-game/app/src/main/java/com/hfad/guessinggame/ResultFragment.kt
+++ playground/25-guessing-game/app/src/main/java/com/hfad/guessinggame/ResultFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.hfad.guessinggame.databinding.FragmentResultBinding
 
@@ -16,6 +17,8 @@ import com.hfad.guessinggame.databinding.FragmentResultBinding
 class ResultFragment : Fragment() {
     private var _binding: FragmentResultBinding? = null
     private val binding get() = _binding!!
+    lateinit var viewModel: ResultViewModel
+    lateinit var viewModelFactory: ResultViewModelFactory
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +28,10 @@ class ResultFragment : Fragment() {
         _binding = FragmentResultBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        binding.wonLost.text = ResultFragmentArgs.fromBundle(requireArguments()).result
+        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
+        viewModelFactory = ResultViewModelFactory(result)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(ResultViewModel::class.java)
+        binding.wonLost.text = viewModel.result
 
         binding.newGameButton.setOnClickListener {
             view.findNavController().navigate(R.id.action_resultFragment_to_gameFragment)
