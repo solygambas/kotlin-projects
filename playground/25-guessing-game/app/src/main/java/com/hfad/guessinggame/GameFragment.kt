f6faa0719ee1cda61e7a2903c053dd86691449d1
solygambas <51904909+solygambas@users.noreply.github.com>
2022-03-28 07:22:44 +0200

    add game logic

--- playground/25-guessing-game/app/src/main/java/com/hfad/guessinggame/GameFragment.kt
+++ playground/25-guessing-game/app/src/main/java/com/hfad/guessinggame/GameFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.hfad.guessinggame.databinding.FragmentGameBinding
 
 /**
@@ -21,7 +22,7 @@ class GameFragment : Fragment() {
     var secretWordDisplay = ""
     var correctGuesses = ""
     var incorrectGuesses = ""
-    var liveLeft = 8
+    var livesLeft = 8
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,9 +32,67 @@ class GameFragment : Fragment() {
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        // TO DO
+        secretWordDisplay = deriveSecretWordDisplay()
+        updateScreen()
+
+        binding.guessButton.setOnClickListener {
+            makeGuess(binding.guess.text.toString().uppercase())
+            binding.guess.text = null
+            updateScreen()
+            if (isWon() || isLost()) {
+                val action = GameFragmentDirections.actionGameFragmentToResultFragment(wonLostMessage())
+                view.findNavController().navigate(action)
+            }
+        }
 
         return view
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun updateScreen() {
+        binding.word.text = secretWordDisplay
+        binding.lives.text = "You have $livesLeft lives left."
+        binding.incorrectGuesses.text = "Incorrect guesses: $incorrectGuesses"
+    }
+
+    private fun deriveSecretWordDisplay() : String {
+        var display = ""
+        secretWord.forEach {
+            display += checkLetter(it.toString())
+        }
+        return display
+    }
+
+    private fun checkLetter(str: String) = when (correctGuesses.contains(str)) {
+        true -> str
+        false -> "_"
+    }
+
+    private fun makeGuess(guess: String) {
+        if (guess.length == 1) {
+            if (secretWord.contains(guess)) {
+                correctGuesses += guess
+                secretWordDisplay = deriveSecretWordDisplay()
+            } else {
+                incorrectGuesses += "$guess "
+                livesLeft--
+            }
+        }
+    }
+
+    private fun isWon() = secretWord.equals(secretWordDisplay, true)
+
+    private fun isLost() = livesLeft <= 0
+
+    private fun wonLostMessage() : String {
+        var message = ""
+        if (isWon()) message = "You won!"
+        else if (isLost()) message = "You lost!"
+        message += " The word was $secretWord"
+        return message
+    }
 }
--- playground/25-guessing-game/app/src/main/java/com/hfad/guessinggame/ResultFragment.kt
+++ playground/25-guessing-game/app/src/main/java/com/hfad/guessinggame/ResultFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.hfad.guessinggame.databinding.FragmentResultBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -12,13 +14,28 @@ import android.view.ViewGroup
  * create an instance of this fragment.
  */
 class ResultFragment : Fragment() {
+    private var _binding: FragmentResultBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        _binding = FragmentResultBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.wonLost.text = ResultFragmentArgs.fromBundle(requireArguments()).result
+
+        binding.newGameButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_resultFragment_to_gameFragment)
+        }
+
+        return view
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
