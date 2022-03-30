aa00f6b935dbefd30a710b07f6e24eaf1d7c63ce
solygambas <51904909+solygambas@users.noreply.github.com>
2022-03-30 11:16:52 +0200

    add grid

--- 07-tasks/app/src/main/java/com/hfad/tasks/TaskItemAdapter.kt
+++ 07-tasks/app/src/main/java/com/hfad/tasks/TaskItemAdapter.kt
@@ -2,7 +2,9 @@ package com.hfad.tasks
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
 class TaskItemAdapter : RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>() {
@@ -21,17 +23,21 @@ class TaskItemAdapter : RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>
         holder.bind(item)
     }
 
-    class TaskItemViewHolder(val rootView: TextView) : RecyclerView.ViewHolder(rootView) {
+    class TaskItemViewHolder(val rootView: CardView) : RecyclerView.ViewHolder(rootView) {
+        val taskName = rootView.findViewById<TextView>(R.id.task_name)
+        val taskDone = rootView.findViewById<CheckBox>(R.id.task_done)
+
         companion object {
-            fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
+                fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.task_item, parent, false) as TextView
+                val view = layoutInflater.inflate(R.layout.task_item, parent, false) as CardView
                 return TaskItemViewHolder(view)
             }
         }
 
         fun bind(item: Task) {
-            rootView.text = item.taskName
+            taskName.text = item.taskName
+            taskDone.isChecked = item.taskDone
         }
     }
 }
