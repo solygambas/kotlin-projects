9a31c51a712e582887583cdff081e983e0a6c3fa
solygambas <51904909+solygambas@users.noreply.github.com>
2022-04-02 14:35:55 +0200

    add converter

--- 08-temperature-converter/app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
+++ 08-temperature-converter/app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
@@ -5,31 +5,97 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.material.Button
+import androidx.compose.material.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
+            /*Column {
                 Hello("friend")
                 Hello("everyone")
-            }
+            }*/
+            MainActivityContent()
+        }
+    }
+
+    /*@Composable
+    fun Hello(name: String) {
+        Text("Hello $name! Welcome to Jetpack Compose")
+    }*/
+
+    @Composable
+    fun Header(image: Int, description: String) {
+        Image (
+            painter = painterResource(image),
+            contentDescription = description,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+    }
+
+    @Composable
+    fun TemperatureText(celsius: Int) {
+        val fahrenheit = (celsius.toDouble()*9/5)+32
+        Text("$celsius Celsius is $fahrenheit Fahrenheit")
+    }
+
+    @Composable
+    fun ConvertButton(clicked: () -> Unit) {
+        Button(onClick = clicked) {
+            Text("Convert")
         }
     }
 
     @Composable
-    fun Hello(name: String) {
-        Text("Hello $name! Welcome to Jetpack Compose")
+    fun EnterTemperature(temperature: String, changed: (String) -> Unit) {
+        TextField(
+            value = temperature,
+            onValueChange = changed,
+            label = { Text("Enter a temperature in Celsius") },
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+
+    @Composable
+    fun MainActivityContent() {
+        val celsius = remember { mutableStateOf(0) }
+        val newCelsius = remember { mutableStateOf("") }
+
+        Column {
+            Header(R.drawable.sunrise, "sunrise image")
+            EnterTemperature(newCelsius.value) { newCelsius.value = it }
+            ConvertButton {
+                newCelsius.value.toIntOrNull()?.let {
+                    celsius.value = it
+                }
+            }
+            TemperatureText(celsius.value)
+        }
     }
 
     @Preview(showBackground = true)
     @Composable
     fun PreviewMainActivity() {
-        Column {
+        /*Column {
             Hello("friend")
             Hello("everyone")
-        }
+        }*/
+        MainActivityContent()
     }
 }
