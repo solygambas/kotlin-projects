45b450a36565d2426180562ba8da40526827ba3a
solygambas <51904909+solygambas@users.noreply.github.com>
2022-04-03 13:40:16 +0200

    refactor GameFragment to use composables

--- 06-guessing-game/app/src/main/java/com/hfad/guessinggame/GameFragment.kt
+++ 06-guessing-game/app/src/main/java/com/hfad/guessinggame/GameFragment.kt
@@ -5,10 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import com.hfad.guessinggame.databinding.FragmentGameBinding
+//import com.hfad.guessinggame.databinding.FragmentGameBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -16,8 +29,8 @@ import com.hfad.guessinggame.databinding.FragmentGameBinding
  * create an instance of this fragment.
  */
 class GameFragment : Fragment() {
-    private var _binding: FragmentGameBinding? = null
-    private val binding get() = _binding!!
+    /*private var _binding: FragmentGameBinding? = null
+    private val binding get() = _binding!!*/
     lateinit var viewModel: GameViewModel
 
     override fun onCreateView(
@@ -25,8 +38,16 @@ class GameFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentGameBinding.inflate(inflater, container, false)
-        val view = binding.root
+        /*_binding = FragmentGameBinding.inflate(inflater, container, false).apply {
+            composeView.setContent {
+                MaterialTheme {
+                    Surface {
+                        GameFragmentContent(viewModel)
+                    }
+                }
+            }
+        }
+        val view = binding.root*/
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         // viewModel.incorrectGuesses.observe(viewLifecycleOwner, Observer { newValue ->
@@ -42,26 +63,101 @@ class GameFragment : Fragment() {
         viewModel.gameOver.observe(viewLifecycleOwner, Observer { newValue ->
             if (newValue) {
                 val action = GameFragmentDirections.actionGameFragmentToResultFragment(viewModel.wonLostMessage())
-                view.findNavController().navigate(action)
+                view?.findNavController()?.navigate(action)
             }
         })
 
         // set the data binding variable
-        binding.gameViewModel = viewModel
+        //binding.gameViewModel = viewModel
         // respond to live data updates
-        binding.lifecycleOwner = viewLifecycleOwner
+        //binding.lifecycleOwner = viewLifecycleOwner
 
-        binding.guessButton.setOnClickListener {
+        /*binding.guessButton.setOnClickListener {
             viewModel.makeGuess(binding.guess.text.toString().uppercase())
             binding.guess.text = null
-        }
+        }*/
 
-        return view
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MaterialTheme {
+                    Surface {
+                        GameFragmentContent(viewModel)
+                    }
+                }
+            }
+        }
     }
 
-    override fun onDestroyView() {
+    @Composable
+    fun SecretWordDisplay(viewModel: GameViewModel) {
+        val display = viewModel.secretWordDisplay.observeAsState()
+        display.value?.let {
+            Text(text = it,
+                letterSpacing = 0.1.em,
+                fontSize = 36.sp)
+        }
+    }
+
+    @Composable
+    fun LivesLeftText(viewModel: GameViewModel) {
+        val livesLeft = viewModel.livesLeft.observeAsState()
+        livesLeft.value?.let {
+            Text(stringResource(R.string.lives_left, it))
+        }
+    }
+
+    @Composable
+    fun IncorrectGuessesText(viewModel: GameViewModel) {
+        val incorrectGuesses = viewModel.incorrectGuesses.observeAsState()
+        incorrectGuesses.value?.let {
+            Text(stringResource(R.string.incorrect_guesses, it))
+        }
+    }
+
+    @Composable
+    fun EnterGuess(guess: String, changed: (String) -> Unit) {
+        TextField(value = guess,
+            onValueChange = changed,
+            label = { Text("Guess a letter") })
+    }
+
+    @Composable
+    fun GuessButton(clicked: () -> Unit) {
+        Button(onClick = clicked) {
+            Text("Guess!")
+        }
+    }
+
+    @Composable
+    fun FinishGameButton(clicked: () -> Unit) {
+        Button(onClick = clicked) {
+            Text("Finish Game")
+        }
+    }
+
+    @Composable
+    fun GameFragmentContent(viewModel: GameViewModel) {
+        val guess = remember { mutableStateOf("") }
+
+        Column(modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            SecretWordDisplay(viewModel)
+            LivesLeftText(viewModel)
+            IncorrectGuessesText(viewModel)
+            EnterGuess(guess.value) { guess.value = it }
+            GuessButton {
+                viewModel.makeGuess(guess.value.uppercase())
+                guess.value = ""
+            }
+            FinishGameButton {
+                viewModel.finishGame()
+            }
+        }
+    }
+
+    /*override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
+    }*/
 
 }
