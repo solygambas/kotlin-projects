fa85b500ea42012fc5604014fa82df7dac64055d
solygambas <51904909+solygambas@users.noreply.github.com>
2022-04-11 12:10:00 +0200

    add details

--- 10-mars-photos/app/src/main/java/com/example/android/marsphotos/detail/DetailViewModel.kt
+++ 10-mars-photos/app/src/main/java/com/example/android/marsphotos/detail/DetailViewModel.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import com.example.android.marsphotos.R
 import com.example.android.marsphotos.network.MarsPhoto
 
 class DetailViewModel(marsProperty: MarsPhoto, app: Application) : AndroidViewModel(app) {
@@ -13,5 +15,23 @@ class DetailViewModel(marsProperty: MarsPhoto, app: Application) : AndroidViewMo
 
     init {
         _selectedProperty.value = marsProperty
+    }
+
+    val displayPropertyPrice = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(
+            when (it.isRental) {
+                true -> R.string.display_price_monthly_rental
+                false -> R.string.display_price
+            }, it.price
+        )
+    }
+
+    val displayPropertyType = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(R.string.display_type,
+        app.applicationContext.getString(
+                when (it.isRental) {
+                    true -> R.string.type_rent
+                    false -> R.string.type_sale
+                }))
     }
 }
