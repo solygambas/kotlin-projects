a92098b66d0d31079f38ede0816e226cd7299a34
solygambas <51904909+solygambas@users.noreply.github.com>
2022-04-11 15:05:42 +0200

    add repository

--- 12-devbytes/app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
+++ 12-devbytes/app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.devbyteviewer.network
 
+import com.example.android.devbyteviewer.database.DatabaseVideo
 import com.example.android.devbyteviewer.domain.DevByteVideo
 import com.squareup.moshi.JsonClass
 
@@ -62,5 +63,16 @@ fun NetworkVideoContainer.asDomainModel(): List<DevByteVideo> {
                 url = it.url,
                 updated = it.updated,
                 thumbnail = it.thumbnail)
+    }
+}
+
+fun NetworkVideoContainer.asDatabaseModel(): List<DatabaseVideo> {
+    return videos.map {
+        DatabaseVideo(
+            title = it.title,
+            description = it.description,
+            url = it.url,
+            updated = it.updated,
+            thumbnail = it.thumbnail)
     }
 }
